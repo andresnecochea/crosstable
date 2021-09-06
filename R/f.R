a3fc2ca1355cf96df906f0182a939d719604ff56
andresnecochea <andres.necochea.v@gmail.com>
2021-09-06 20:24:40 -0300

    Improvement in %f% function

--- R/f.R
+++ R/f.R
@@ -1,14 +1,16 @@
 # Binary operator to give format to a number
 "%f%" <- function(x,f) {
-  prefix <- sub("(.*)(#+.{1}#+)(0+.?0*)(.*)", "\\1", f)
-  suffix <- sub("(.*)(#+.{1}#+)(0+.?0*)(.*)", "\\4", f)
-  sub("(.*)(#+.{1}#+)(0+.?0*)(.*)", "\\2", f) %>%
+  numFormat <- sub("^[^0]*(0+.?0*)[^0]*$", "\\1", f)
+  prefix <- sub("(#.?#*)?0{1}(.{1}0+)?.*","",f)
+  suffix <- sub(".*(#.?#*)?0{1}(.{1}0+)?","",f)
+  sub("^.*(#.#)0.*$", "\\1", f) %>%
     gsub("#", "", .) -> big.mark
-  sub("(.*)(#+.{1}#+)(0+.?0*)(.*)", "\\3", f) %>%
-    gsub("0", "", .) -> decimal.mark
-  sub("(.*)(#+.{1}#+)(0+.?0*)(.*)", "\\3", f) %>%
-    sub("(0+.?)(0*)", "\\2", .) %>%
+  big.mark <- c("", big.mark)[as.numeric(grepl("^.*#.#0.*", f))+1]
+  decimal.mark <- gsub("0", "", numFormat)
+  decimal.mark <- c(getOption("OutDec"), decimal.mark)[as.numeric(grepl("^.*0.0+.*$", numFormat))+1]
+  sub("(0+.?)(0*)", "\\2", numFormat) %>%
     nchar -> digits
-  format(x,nsmall=digits ,big.mark=big.mark, decimal.mark=as.character(decimal.mark)[1]) %>%
+  round(x, digits) %>%
+    format(x,nsmall=digits ,big.mark=big.mark, decimal.mark=as.character(decimal.mark)[1]) %>%
     paste0(prefix, ., suffix)
 }
