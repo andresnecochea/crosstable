dca6beb163054ee990fa07befa5a2a25ccb48d73
andresnecochea <83134257+andresnecochea@users.noreply.github.com>
2021-09-30 09:50:58 -0300

    fix in %f% function

--- R/print.crosstable.R
+++ R/print.crosstable.R
@@ -27,19 +27,8 @@ print.crosstable <- function(x) {
   #-------------------------#
   # MAKE THE TABLE TO PRINT #
   #-------------------------#
-  dimTable <- dim(tablePrint)
-
-  # Index of original table to make
-  s <- sapply(1:prod(dimTable[(length(dimTable)-1):length(dimTable)]),rep,prod(dimTable[-((length(dimTable)-1):length(dimTable))]))
-  dim(s) <- dimTable
-  s <- sweep(s, max(c(row.vars, col.vars)), 0, "+")
-  s <- sort(s, index.return=TRUE)$ix
-
+  # Format the rows and columns of the table
   tablePrint <- sweep(tablePrint, max(c(row.vars, col.vars)), arguments$format, "%f%")
-
-  # Now we sort the table using the s index
-  tablePrint <- as.vector(tablePrint)[s]
-  dim(tablePrint) <- dimTable
 
   # Define the Column Headers
   colHeaders <- tableDimNames[col.vars]
